066f873669a8f3be37a1cf2e1c5109489f9414fb
Eli Hartnett <55256366+Elichartnett@users.noreply.github.com>
2020-09-23 23:24:00 -0500

    Add files via upload

--- WebApp/main.py
+++ WebApp/main.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+port=5000
+
 @app.route('/')
-def home():
+def login():
    return render_template('login.html')
 
 
 @app.route('/inbox')
-def test():
+def inbox():
    return render_template('inbox.html')
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(host = '0.0.0.0')  # Launches server on main computer's ipv4 address:5000
