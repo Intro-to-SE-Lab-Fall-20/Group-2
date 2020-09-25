38d4acc2db8af6d1382fb87fc041076487418787
Sheldon Waid <69919496+Swaid-design@users.noreply.github.com>
2020-09-24 21:35:21 -0500

    Add files via upload

--- WebApp/main.py
+++ WebApp/main.py
@@ -1,17 +1,52 @@
 from flask import Flask, render_template
+from flask import Flask, request
 
 app = Flask(__name__)
 
 port=5000
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-   return render_template('login.html')
 
+	if request.method == 'POST':
+		print(request.form['email'])
+		return inbox()
+	else:
+		return render_template('login.html')
 
-@app.route('/inbox')
+@app.route('/', methods=['POST', 'GET'])
 def inbox():
-   return render_template('inbox.html')
+	return render_template('inbox.html')
+
+
+@app.route('/sendmail', methods=['GET', 'POST'])
+def sendmail():
+	print("HERE4")
+	if request.method == 'POST':
+		print("HERE6")
+		toEmail = request.form['toemail']
+		subject = request.form['subject']
+		msg = request.form['msgbody']
+		print(toEmail, " ", subject, " ", msg)
+		return sendemail(toEmail, subject, msg)
+	else:
+		print("HERE5")
+		return render_template('sendmail.html')
+
+
+
+
+
+
+
+
+
+
+def sendemail(toEmail, subject, msg):
+	print(toEmail, " ", subject, " ", msg)
+	return inbox()
+
+
 
 if __name__ == '__main__':
    app.run(host = '0.0.0.0')  # Launches server on main computer's ipv4 address:5000
