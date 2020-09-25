6cf1e3060e55ae653e4a18218f86565f711a5d52
Sheldon Waid <69919496+Swaid-design@users.noreply.github.com>
2020-09-24 23:44:59 -0500

    Add files via upload

--- WebApp/main.py
+++ WebApp/main.py
@@ -1,50 +1,54 @@
 from flask import Flask, render_template
 from flask import Flask, request
+import smtplib, ssl #server library
 
 app = Flask(__name__)
+
+emailport = 465 #Gmail port
+
+context = ssl.create_default_context() #idk what this does
 
 port=5000
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
 
-	if request.method == 'POST':
-		print(request.form['email'])
-		return inbox()
+	if request.method == 'POST': #renders login.html until form is submitted
+		global userEmail	#when submitted takes in these values
+		userEmail = request.form['email']
+		global userPsswrd     #global because they are needed in another function
+		userPsswrd = request.form['password']  #requests the object with name 'password'
+		return login2(userEmail, userPsswrd)
 	else:
 		return render_template('login.html')
 
+def login2(email, password):
+	with smtplib.SMTP_SSL("smtp.gmail.com", emailport, context = context) as server:
+		server.login(email, password) #connecting to server and logging in(checks creds)
+		return inbox()
+
 @app.route('/', methods=['POST', 'GET'])
 def inbox():
-	return render_template('inbox.html')
+	return render_template('inbox.html') #when sendmail button is clicked it performs the action(sendmail)
 
 
-@app.route('/sendmail', methods=['GET', 'POST'])
+@app.route('/sendmail', methods=['GET', 'POST']) #"/sendmail" is the action
 def sendmail():
-	print("HERE4")
 	if request.method == 'POST':
-		print("HERE6")
 		toEmail = request.form['toemail']
-		subject = request.form['subject']
+		subject = request.form['subject'] #not really needed
 		msg = request.form['msgbody']
-		print(toEmail, " ", subject, " ", msg)
 		return sendemail(toEmail, subject, msg)
 	else:
-		print("HERE5")
 		return render_template('sendmail.html')
 
 
-
-
-
-
-
-
-
-
 def sendemail(toEmail, subject, msg):
-	print(toEmail, " ", subject, " ", msg)
-	return inbox()
+	with smtplib.SMTP_SSL("smtp.gmail.com", emailport, context = context) as server:
+		server.login(userEmail, userPsswrd)  #for somereason i had to log in again before sending
+		server.sendmail(userEmail, toEmail, msg)
+	print(toEmail, " ", subject, " ", msg) #prints to terminal
+	return inbox() #goes back to inbox after sending
 
 
 
