6d0e55873badbd08c3217f93d1cb2019d314623b
Sheldon Waid <69919496+Swaid-design@users.noreply.github.com>
2020-10-12 12:46:28 -0500

    Add files via upload
    Logout function in main.py, has directions on where to copy and paste code.

--- WebApp/logout.py
+++ WebApp/logout.py
@@ -1,4 +1,17 @@
+
+import os
+
+#copy and paste right above if __name__ ==
+# in main.py
 @app.route('/logout')
 def logout():
     os.remove('userCredentials.txt')
     return redirect('/')
+
+
+#copy and paste into line 149 in main.py
+"<form action=\"logout\">"
+"<button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Logout</button>\n"
+"</form>\n"
+
+
