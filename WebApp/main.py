30421150585f341a44c8a735fa8d7074b455a4ea
Eli Hartnett <55256366+Elichartnett@users.noreply.github.com>
2020-10-18 10:48:39 -0500

    Add files via upload
    Can now click on an email and load the body. Not 100% done, but uploading so Sheldon can begin forwarding function using new file

--- WebApp/main.py
+++ WebApp/main.py
@@ -46,7 +46,6 @@ def travisTest():  # sends email to itself to verify it works (for travis CI)
     if time in body: # if email time is same as the time the test email was sent, test passes
         exit()
 
-
 # Web Pages - first pages are commented, the rest follow similar functionality
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -64,19 +63,58 @@ def login():
     else:
         return render_template('login.html')  # renders login.html until form is submitted
 
-
 @app.route('/inbox', methods=['POST', 'GET'])
 def inbox():
-    if request.method == 'POST':
-        userSearch = request.form['search']  # requests the object with name 'search'
+    userInfoFile = open("userCredentials.txt", 'r')
+    userEmail = userInfoFile.readline()
+    userPassword = userInfoFile.readline()
+    userInfoFile.close()
 
-        print(userSearch)
-        return render_template('/SearchResults.html')
+    if request.method == 'POST':
+        # create an IMAP4 class with SSL
+        imap = imaplib.IMAP4_SSL("imap.gmail.com")
+        # authenticate
+        imap.login(userEmail, userPassword)
+        imap.select('Inbox')
+        type, messages = imap.search(None, 'ALL')
+        numEmails = len(messages[0].split())
+
+        for i in range(numEmails):
+            if request.form.get(str(i)):
+                typ, data = imap.fetch(str(i), '(RFC822)')
+                for response_part in data:
+                    if isinstance(response_part, tuple):
+                        msg = email.message_from_string(response_part[1].decode('latin1'))
+                        email_subject = msg['subject']
+                        if msg['subject'] != None:
+                            email_subject = msg['subject']
+                        email_from = msg['from']
+                        email_date = msg['date']
+                        email_body = msg.get_payload()
+
+                        text = "<p>Sender: " + email_from + "</p>\n"
+                        text += "<p>Subject: " + email_subject + "</p>\n"
+                        text += "<p>Date: " + email_date + "</p>\n"
+                        if len(email_body) == 0 or len(email_body) == 1: # *ELI* will work on this
+                            text += "<p>Message: " + email_body + "</p>" # need to check if body
+                        else:                                            # is a list.
+                            print("too long to display")
+                            print(len(email_body))
+
+                        file = open("templates/displayEmail.html", 'w')
+                        file.write(text)
+                        file.close()
+                return render_template('displayEmail.html')
+
+            elif request.form.get('search'):
+                userSearch = request.form.get('search')  # requests the object with name 'search'
+
+                print(userSearch)
+                return render_template('/SearchResults.html')
 
     else:
         loadInbox()
         return render_template('inbox.html')  # renders inbox.html until form is submitted
-
 
 @app.route('/sendmail', methods=['GET', 'POST'])
 def sendMail():
@@ -115,8 +153,6 @@ def sendMail():
     else:
         return render_template('sendmail.html')
 
-
-
 @app.route('/logout')
 def logout():
     os.remove('userCredentials.txt')
@@ -141,51 +177,48 @@ def authenticate():
     with smtplib.SMTP_SSL("smtp.gmail.com", emailport, context=context) as server:
         server.login(userEmail, userPassword)  # connecting to server and logging in (checks creds)
 
-
 def loadInbox():
     userInfoFile = open("userCredentials.txt", 'r')
     userEmail = userInfoFile.readline()
     userPassword = userInfoFile.readline()
     userInfoFile.close()
 
-    htmlFile = open("templates/inbox.html", 'w')
-    htmlFile.write(
-        "<!doctype html>\n"
-        "<html lang=\"en\">\n"
-        "<head>\n"
+    text = (
+    "<!doctype html>\n"
+    "<html lang=\"en\">\n"
+    "<head>\n"
 
-        "<meta charset=\"utf-8\">\n"
-        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n"
+    "<meta charset=\"utf-8\">\n"
+    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n"
 
-        "<title>Email Client</title>\n"
+    "<title>Email Client</title>\n"
 
-        "<!-- Bootstrap core CSS -->\n"
-        "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\">\n"
+    "<!-- Bootstrap core CSS -->\n"
+    "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\">\n"
 
-        "<link rel= \"stylesheet\" type= \"text/css\" href= \"{{ url_for('static',filename='styles/styles.css') }}\">\n"
+    "<link rel= \"stylesheet\" type= \"text/css\" href= \"{{ url_for('static',filename='styles/styles.css') }}\">\n"
 
-        "</head>\n"
-        "<body class=\"text-center\">\n"
-        "<div class=\"container\">\n"
-        "<form action=\"logout\">"
-        "<button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Logout</button>\n"
-        "</form>\n"
-        "<h1>Inbox</h1>\n"
-        "<form class=\"inbox\" method=\"POST\">\n"
-        "<label for=\"searchInbox\" class=\"sr-only\">Search Term</label>\n"
-        "<input type=\"search\" name= \"search\" id=\"searchInbox\" class=\"form-control\" placeholder=\"Search Inbox\" required autofocus> \n"
-        "<button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Search Inbox</button> \n"
-        "</form>"
-        "<table class=\"table\">\n"
-        "<thead class=\"thead-dark\">\n"
-        "<tr>\n"
-        "<th scope=\"col\">Sender</th>\n"
-        "<th scope=\"col\">Subject</th>\n"
-        "<th scope=\"col\">Time</th>\n"
-        "</tr>\n"
-        "</thead>\n"
-        "<tbody>\n")
-    htmlFile.close()
+    "</head>\n"
+    "<body class=\"text-center\">\n"
+    "<div class=\"container\">\n"
+    "<form action=\"logout\">"
+    "<button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Logout</button>\n"
+    "</form>\n"
+    "<h1>Inbox</h1>\n"
+    "<form class=\"inbox\" method=\"POST\">\n"
+    "<label for=\"searchInbox\" class=\"sr-only\">Search Term</label>\n"
+    "<input type=\"search\" name= \"search\" id=\"searchInbox\" class=\"form-control\" placeholder=\"Search Inbox\" required autofocus> \n"
+    "<button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Search Inbox</button> \n"
+    "</form>"
+    "<table class=\"table\">\n"
+    "<thead class=\"thead-dark\">\n"
+    "<tr>\n"
+    "<th scope=\"col\">Sender</th>\n"
+    "<th scope=\"col\">Subject</th>\n"
+    "<th scope=\"col\">Time</th>\n"
+    "</tr>\n"
+    "</thead>\n"
+    "<tbody>\n")
 
     userInfoFile = open("userCredentials.txt", 'r')
     userEmail = userInfoFile.readline()
@@ -225,43 +258,41 @@ def loadInbox():
                 email_body = msg.get_payload()
                 index-=1
 
-        htmlFile = open("templates/inbox.html", 'a')  # appending sender, subject, and time to inbox.html file
-        htmlFile.write(
+        text += (  # appending sender, subject, and time to inbox.html file
             "<tr>\n"
             "<td>")
-        htmlFile.close()
 
-        htmlFile = open("templates/inbox.html", 'a')
-        htmlFile.write(email_from)
-        htmlFile.close()
+        text += email_from
 
-        htmlFile = open("templates/inbox.html", 'a')
-        htmlFile.write(
+        text += (
             "</td>\n"
             "<td>")
-        htmlFile.close()
 
         if email_subject != None:
-            htmlFile = open("templates/inbox.html", 'a')
-            htmlFile.write(email_subject)
-            htmlFile.close()
+            text += email_subject
 
-        htmlFile = open("templates/inbox.html", 'a')
-        htmlFile.write("</td>\n"
-                       "<td>")
-        htmlFile.close()
+        text += (
+            "</td>\n"
+            "<td>")
 
-        htmlFile = open("templates/inbox.html", 'a')
-        htmlFile.write(email_date)
-        htmlFile.close()
+        text += email_date
 
-        htmlFile = open("templates/inbox.html", 'a')
-        htmlFile.write("</td>\n"
-                       "<tr>\n")
-        htmlFile.close()
+        text += (
+            "<form method =\"post\" action=\"/inbox\">" 
+                 "<button type=\"submit\" name=\"")
+        text += str(index)
+        text += "\" value=\""
+        text += str(index)
+        text += "\"> Open Message "
+        text += str(index)
+        text += "</button>"
 
-    htmlFile = open("templates/inbox.html", 'a')
-    htmlFile.write(
+
+        text += (
+            "</td>\n"
+            "<tr>\n")
+
+    text += (
         "</tr>\n"
         "</tbody>\n"
         "</table>\n"
@@ -275,6 +306,8 @@ def loadInbox():
         "</body>\n"
         "</html>\n")
 
+    htmlFile = open("templates/inbox.html", 'w')
+    htmlFile.write(text)
 
 def sendEmail(newMessage):
     userInfoFile = open("userCredentials.txt", 'r')
