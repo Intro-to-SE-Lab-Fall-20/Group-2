ceeca3e5672815183fba6302124787e9f39b7126
Sheldon Waid <69919496+Swaid-design@users.noreply.github.com>
2020-11-12 13:05:53 -0600

    Add files via upload
    fixed navigation bugs

--- WebApp/main.py
+++ WebApp/main.py
@@ -25,8 +25,6 @@ app = Flask(__name__)
 emailport = 465  # Gmail port
 context = ssl.create_default_context()
 
-global key
-key = Fernet.generate_key()
 error = ''
 
 
@@ -42,7 +40,42 @@ def travisTest():  # sends email to itself to verify it works (for travis CI)
         except:
             print("PASS")
 
-    print("Test 2: Login with correct credentials.")  # setting up email
+    print("Test 2: Login with wrong password/correct email.")
+    userEmail = "group2emailclient@gmail.com"
+    userPassword = "wrongPassword1"
+    with smtplib.SMTP_SSL("smtp.gmail.com", emailport, context=context) as server:  # sending email
+        try:
+            server.login(userEmail, userPassword)
+            print("FAIL")
+        except:
+            print("PASS")
+
+    print("Test 3: Login with wrong email/correct password.")
+    userEmail = "wrongEmail@gmail.com"
+    userPassword = "Group2Test"
+    with smtplib.SMTP_SSL("smtp.gmail.com", emailport, context=context) as server:  # sending email
+        try:
+            server.login(userEmail, userPassword)
+            print("FAIL")
+        except:
+            print("PASS")
+
+    print("Test 4: Locking out the user.")
+    userEmail = "wrongEmail@gmail.com"
+    userPassword = "wrongPassword1"
+    with smtplib.SMTP_SSL("smtp.gmail.com", emailport, context=context) as server:  # sending email
+        i = 0
+        while(i < 5):
+            try:
+                server.login(userEmail, userPassword)
+                Status = "FAIL"
+                i += 1
+            except:
+                Status = "PASS"
+                i += 1
+        print(Status)
+
+    print("Test 5: Login with correct credentials.")  # setting up email
     userEmail = "group2emailclient@gmail.com"
     userPassword = "Group2Test"
 
@@ -53,7 +86,7 @@ def travisTest():  # sends email to itself to verify it works (for travis CI)
         except:
             print("FAIL")
 
-    print("Test 3: Create email with no sender, but includes a subject and body.")
+    print("Test 6: Create email with no sender, but includes a subject and body.")
     newMessage = EmailMessage()
     newMessage['To'] = ""
     newMessage['Subject'] = "TRAVIS CI TEST"
@@ -62,7 +95,8 @@ def travisTest():  # sends email to itself to verify it works (for travis CI)
     time = str(time)
     newMessage.set_content("Verification time: " + time)
     print("PASS")
-    print("Test 4: Send test 3 email to itself.")
+
+    print("Test 7: Send test 6 email to itself.")
     with smtplib.SMTP_SSL("smtp.gmail.com", emailport, context=context) as server:
         try:
             server.login(userEmail, userPassword)
@@ -71,7 +105,7 @@ def travisTest():  # sends email to itself to verify it works (for travis CI)
         except:
             print("PASS")
 
-    print("Test 5: Create email with sender, subject, and body.")
+    print("Test 8: Create email with sender, subject, and body.")
     newMessage = EmailMessage()
     newMessage['To'] = userEmail
     newMessage['Subject'] = "TRAVIS CI TEST"
@@ -80,12 +114,14 @@ def travisTest():  # sends email to itself to verify it works (for travis CI)
     time = str(time)
     newMessage.set_content("Verification time: " + time)
     print("PASS")
-    print("Test 6: Send test 5 email to itself.")
+
+    print("Test 9: Send test 8 email to itself.")
     with smtplib.SMTP_SSL("smtp.gmail.com", emailport, context=context) as server:  # sending email
         server.login(userEmail, userPassword)
         server.send_message(newMessage)
         print("PASS")
-    print("Test 7: Check if that exact previous email was recieved.")
+
+    print("Test 10: Check if that exact previous email was recieved.")
     # create an IMAP4 class with SSL
     imap = imaplib.IMAP4_SSL("imap.gmail.com")
     imap.login(userEmail, userPassword)
@@ -96,6 +132,36 @@ def travisTest():  # sends email to itself to verify it works (for travis CI)
     msg = email.message_from_string(data[0][1].decode('latin1'))
     body = msg.get_payload()
     if time in body: # if email time is same as the time the test email was sent, test passes
+        print("PASS")
+
+
+    print("Test 11: Navigating to inbox without logging in.")
+    try:
+        redirect("/inbox")
+        if authenticate() == True:
+            print("FAIL")
+        else: 
+            print("PASS")
+    except:
+        print("PASS")
+    print("Test 12: Navigating to director without logging in.")
+    try:
+        redirect("/director")
+        if authenticate() == True:
+            print("FAIL")
+        else: 
+            print("PASS")
+    except:
+        print("PASS")
+
+    print("Test 13: Navigating to notes without logging in.")
+    try:
+        redirect("/notes")
+        if authenticate() == True:
+            print("FAIL")
+        else: 
+            print("PASS")
+    except:
         print("PASS")
     exit()
 
@@ -109,10 +175,8 @@ def login():
     if request.method == 'POST':  # when form is submitted, it collects the data and authenticates
         userEmail = request.form['email']  # requests the object with name 'email'
         userPassword = request.form['password']  # requests the object with name 'password'
-        #userPassword = encrypt(userPassword.encode(), key)
         userInfoFile = open("userCredentials.txt", 'w')  # saving credentials for later use
         userInfoFile.write(userEmail + "\n")
-        #userInfoFile = open("userCredentials.txt", 'ab')
         userInfoFile.write(userPassword + "\n")
         userInfoFile.close()
 
@@ -142,56 +206,60 @@ def login():
 
 @app.route('/director', methods=['POST', 'GET'])
 def direct():
-    if request.method == "POST":
-        if "Notes" in request.form:
-            return notes()
-        if "Email" in request.form:
-            return redirect('/inbox')
+    if authenticate() == True:
+        if request.method == "POST":
+            if "Notes" in request.form:
+                return redirect('/notes')
+            if "Email" in request.form:
+                return redirect('/inbox')
 
-    else:
-        return render_template('director.html')
+        else:
+            return render_template('director.html')
+    else: return redirect("/")
 
 @app.route('/notes', methods=['POST', 'GET'])
 def notes():
-    text = """<!DOCTYPE html>\n
-            <html lang="en">\n
-            <head>\n
-            <meta charset="utf-8">\n
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\n
-            <title>Email Client</title>\n
-            <script src=\"//cdn.ckeditor.com/4.14.1/basic/ckeditor.js\">\n</script>\n
-            <!-- Bootstrap core CSS -->\n
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">\n
-          </head>\n
-          <body class="text-center">\n
-        <form action="logout">
-        <button class="btn btn-primary" style="right: 0;" type="submit">Logout</button>\n
-        </form>\n
-          <div style="height: 600px; width: 125px; position: absolute; left: 50px; top: 100px;">\n"""
+    if authenticate() == True:
+        text = """<!DOCTYPE html>\n
+                <html lang="en">\n
+                <head>\n
+                <meta charset="utf-8">\n
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\n
+                <title>Email Client</title>\n
+                <script src=\"//cdn.ckeditor.com/4.14.1/basic/ckeditor.js\">\n</script>\n
+                <!-- Bootstrap core CSS -->\n
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">\n
+              </head>\n
+              <body class="text-center">\n
+            <form action="logout">
+            <button class="btn btn-primary" style="right: 0;" type="submit">Logout</button>\n
+            </form>\n
+              <div style="height: 600px; width: 125px; position: absolute; left: 50px; top: 100px;">\n"""
 
-    if path.exists("notes.txt"):
-        more = True
-        body =''
-        with open("notes.txt", "r") as notes:
-            line = notes.readline()
-            while "eon" not in line:
-                print("this many times")
-                if "T-" in line:
-                    title = line[2:-1]
-                    line = notes.readline()
-                    body = line[2:-1]
-                    print("Title: ", title)
-                    print("Body: ", body)
-                    line = notes.readline()
-                    text += ("<button class=\"btn btn-lg btn-primary\" style=\"width: 125px; margin: 2.5px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;\" onclick=\""
-                    "document.getElementById(\'Title\').value = \'" + title + "\';\n"
-                    "CKEDITOR.instances.Message.setData(\'" + body + "\');\n"
-                    "\">" + title + "</button>\n")
-                if "eon" in line:
-                    break
-
-                
+        if path.exists("notes.txt"):
+            more = True
+            body =''
+            with open("notes.txt", "r") as notes:
                 line = notes.readline()
+                while "eon" not in line:
+                    print("this many times")
+                    if "T-" in line:
+                        title = line[2:-1]
+                        line = notes.readline()
+                        body = line[2:-1]
+                        print("Title: ", title)
+                        print("Body: ", body)
+                        line = notes.readline()
+                        text += ("<button class=\"btn btn-lg btn-primary\" style=\"width: 125px; margin: 2.5px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;\" onclick=\""
+                        "document.getElementById(\'Title\').value = \'" + title + "\';\n"
+                        "CKEDITOR.instances.Message.setData(\'" + body + "\');\n"
+                        "\">" + title + "</button>\n")
+                    if "eon" in line:
+                        break
+
+                    
+                    line = notes.readline()
+    else: return redirect("/")
 
 
     text += """
@@ -219,6 +287,8 @@ def notes():
 
 @app.route('/inbox', methods=['POST', 'GET'])
 def inbox():
+    if authenticate() == False:
+        return redirect("/")
     userInfoFile = open("userCredentials.txt", 'r')
     userEmail = userInfoFile.readline()
     userPassword = userInfoFile.readline()
@@ -251,6 +321,8 @@ def inbox():
 
 @app.route('/sendmail', methods=['GET', 'POST'])
 def sendMail():
+    if authenticate() == False:
+        return redirect("/")
     userInfoFile = open("userCredentials.txt", 'r')
     userEmail = userInfoFile.readline()
     userPassword = userInfoFile.readline()
@@ -762,9 +834,6 @@ def pageforward():
 
     loadInbox(search, index)
     return render_template('inbox.html')
-
-def encrypt(token: bytes, key: bytes) -> bytes:
-    return Fernet(key).encrypt(token)
 
 @app.route('/savenote', methods=['POST', 'GET'])
 def savenote():
